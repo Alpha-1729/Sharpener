060401ef8027001424dd7cb8a6caa15e98970161
Alpha-1729 <10110011100011110000@tutanota.com>
2024-11-01 13:53:40 +0530

    Writing Complex JSX

---
+++ 005-Lets_Get_Started_With_React/004-Writing_Complex_Jsx/my-app/src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    return <>
+        <div>Book</div>
+        <div>$10</div>
+        <div>15 August 2023</div>
+        <div>Delhi</div>
+    </>;
+}
+
+export default ExpenseItem;
