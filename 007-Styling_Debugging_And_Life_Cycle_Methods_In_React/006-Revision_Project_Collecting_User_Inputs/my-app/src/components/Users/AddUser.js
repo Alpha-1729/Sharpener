c7389ecd32c38fca8dcad6b933e0810cfde0be5e
Alpha-1729 <10110011100011110000@tutanota.com>
2024-11-09 12:23:39 +0530

    Revision Project - Collecting User Inputs

---
+++ 007-Styling_Debugging_And_Life_Cycle_Methods_In_React/006-Revision_Project_Collecting_User_Inputs/my-app/src/components/Users/AddUser.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import "./AddUser.css";
+
+const AddUser = () => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        console.log(username, age);
+        setAge("");
+        setUserName("");
+    };
+
+    function usernameChangeHandler(event) {
+        setUserName(event.target.value);
+    }
+    function ageChangeHandler(event) {
+        setAge(event.target.value);
+    }
+
+    const [age, setAge] = useState("");
+    const [username, setUserName] = useState("");
+
+    return (
+        <Card className="input">
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" value={username} onChange={usernameChangeHandler} />
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number" value={age} onChange={ageChangeHandler} />
+                <Button type="submit" >Add User</Button>
+            </form>
+        </Card>
+    );
+};
+
+export default AddUser;
