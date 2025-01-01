54ca5e6f03a55b829b21e42be4a43c19391d7e8e
Alpha-1729 <10110011100011110000@tutanota.com>
2025-01-01 18:07:38 +0530

    Add logic to increase and decrease the cart Item

--- 009-Creating_A_Restaurant_Website/008-Diy_Showing_And_Enabling_The_Delete_Of_Cart_Items/my-app/src/store/CartProvider.js
+++ 009-Creating_A_Restaurant_Website/008-Diy_Showing_And_Enabling_The_Delete_Of_Cart_Items/my-app/src/store/CartProvider.js
@@ -7,15 +7,50 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-    if (action.type == 'ADD') {
-        const updatedItems = state.items.concat(action.item);
+    if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+        if (existingCartItemIndex !== -1) {
+            const updateItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updateItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
-    } else if (action.tu)
-        return defaultCartState;
+    }
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
 };
 
 function CartProvider(props) {
@@ -36,7 +71,6 @@ function CartProvider(props) {
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
-
 
     return <CartContext.Provider value={cartContext}>
         {props.children}
