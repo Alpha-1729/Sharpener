2a2461d580dc4d08f11272ede0a70ad7820ffd7f
Alpha-1729 <10110011100011110000@tutanota.com>
2025-01-17 01:46:07 +0530

    Verify User Email

---
+++ 013-Expense_Tracker/005-Verify_User_Email/my-app/src/pages/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ExpenseTracker from "../components/ExpenseTracker";
+
+function HomeScreen() {
+    return <>
+        <ExpenseTracker />
+    </>
+}
+
+export default HomeScreen;
